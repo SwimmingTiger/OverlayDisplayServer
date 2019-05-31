32374e65bd36b727fd2fabe0cba1f449baa057be
SwimmingTiger <hu60.cn@gmail.com>
2019-06-01 01:12:36 +0800

    add MIT license.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -2,6 +2,7 @@
 MIT License
 
 Copyright (c) 2018 Benjamin Höglinger
+Copyright (c) 2019 SwimmingTiger
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
